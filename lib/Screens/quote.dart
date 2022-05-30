4dcaeff7eedc99d87b0888702b97aa7113527a04
Dhruv Devasthale <dhruv_devasthale@outlook.com>
2022-05-30 15:08:55 +0530

    minor ui changes for quote screen

--- lib/Screens/quote.dart
+++ lib/Screens/quote.dart
@@ -28,8 +28,12 @@ class _QuoteState extends State<Quote> {
         leading: IconButton(
           icon: const Icon(Icons.arrow_back_ios),
           onPressed: () {
-            Navigator.pushReplacement(context,
-                MaterialPageRoute(builder: (builder) => const HomePage(),),);
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (builder) => const HomePage(),
+              ),
+            );
           },
         ),
         centerTitle: true,
@@ -54,59 +58,58 @@ class _QuoteState extends State<Quote> {
             SizedBox(
               height: 50,
               child: Container(
-                  color: Colors.white,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          setState(() {
-                            price = true;
-                          });
-                        },
-                        child: Container(
-                          width: MediaQuery.of(context).size.width * 0.40,
-                          margin: const EdgeInsets.all(5),
-                          color: price ? Colors.white : Colors.black,
-                          child: Center(
-                            child: Text(
-                              "Price",
-                              style: TextStyle(
-                                  color: price
-                                      ? Colors.black
-                                      : Colors.white,
-                                  fontSize: 22,
-                                  fontWeight: FontWeight.w600,),
+                color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          price = true;
+                        });
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.40,
+                        margin: const EdgeInsets.all(5),
+                        color: price ? Colors.white : Colors.black,
+                        child: Center(
+                          child: Text(
+                            "Price",
+                            style: TextStyle(
+                              color: price ? Colors.black : Colors.white,
+                              fontSize: 22,
+                              fontWeight: FontWeight.w600,
                             ),
                           ),
                         ),
                       ),
-                      InkWell(
-                        onTap: () {
-                          setState(() {
-                            price = false;
-                          });
-                        },
-                        child: Container(
-                          color: !price ? Colors.white : Colors.black,
-                          width: MediaQuery.of(context).size.width * 0.40,
-                          margin: const EdgeInsets.all(5),
-                          child: Center(
-                            child: Text(
-                              "Quantity",
-                              style: TextStyle(
-                                  color: !price
-                                      ? Colors.black
-                                      : Colors.white,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w600,),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          price = false;
+                        });
+                      },
+                      child: Container(
+                        color: !price ? Colors.white : Colors.black,
+                        width: MediaQuery.of(context).size.width * 0.40,
+                        margin: const EdgeInsets.all(5),
+                        child: Center(
+                          child: Text(
+                            "Quantity",
+                            style: TextStyle(
+                              color: !price ? Colors.black : Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
                             ),
                           ),
                         ),
                       ),
-                    ],
-                  ),),
+                    ),
+                  ],
+                ),
+              ),
             ),
             const SizedBox(
               height: 25,
@@ -117,8 +120,10 @@ class _QuoteState extends State<Quote> {
               children: [
                 GestureDetector(
                   onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Settings()),);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Settings()),
+                    );
                   },
                   child: Consumer<HomeStateNotifier>(
                     builder: (context, notifier, _) {
@@ -128,7 +133,8 @@ class _QuoteState extends State<Quote> {
                             radius: 25,
                             backgroundColor: Colors.blue,
                             backgroundImage: NetworkImage(
-                                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoG97VgQYJGXN8kDJkOMvh79mgLvO5iEfVWA&usqp=CAU",),
+                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoG97VgQYJGXN8kDJkOMvh79mgLvO5iEfVWA&usqp=CAU",
+                            ),
                           ),
                           Text(
                             notifier.state.selectedCurrencyKey.toUpperCase(),
@@ -147,16 +153,17 @@ class _QuoteState extends State<Quote> {
                       style: TextStyle(color: Colors.white, fontSize: 18),
                     ),
                     Switch(
-                        activeColor: Colors.green,
-                        activeTrackColor: Colors.lightGreen,
-                        inactiveThumbColor: Colors.grey,
-                        inactiveTrackColor: Colors.black,
-                        value: toggle,
-                        onChanged: (value) {
-                          setState(() {
-                            toggle = value;
-                          });
-                        },),
+                      activeColor: Colors.green,
+                      activeTrackColor: Colors.lightGreen,
+                      inactiveThumbColor: Colors.grey,
+                      inactiveTrackColor: Colors.black,
+                      value: toggle,
+                      onChanged: (value) {
+                        setState(() {
+                          toggle = value;
+                        });
+                      },
+                    ),
                   ],
                 ),
               ],
@@ -169,6 +176,7 @@ class _QuoteState extends State<Quote> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Column(
+                  mainAxisSize: MainAxisSize.min,
                   children: const [
                     Text(
                       "Cost Price",
@@ -181,9 +189,7 @@ class _QuoteState extends State<Quote> {
                     SizedBox(
                       height: 15,
                     ),
-                    SizedBox(
-                      height: 20,
-                      width: 120,
+                    Flexible(
                       child: Center(
                         child: Text(
                           '10,00,000',
@@ -201,16 +207,17 @@ class _QuoteState extends State<Quote> {
                       style: TextStyle(color: Colors.white, fontSize: 18),
                     ),
                     Switch(
-                        activeColor: Colors.green,
-                        activeTrackColor: Colors.lightGreen,
-                        inactiveThumbColor: Colors.grey,
-                        inactiveTrackColor: Colors.black,
-                        value: toggle,
-                        onChanged: (value) {
-                          setState(() {
-                            toggle = value;
-                          });
-                        },),
+                      activeColor: Colors.green,
+                      activeTrackColor: Colors.lightGreen,
+                      inactiveThumbColor: Colors.grey,
+                      inactiveTrackColor: Colors.black,
+                      value: toggle,
+                      onChanged: (value) {
+                        setState(() {
+                          toggle = value;
+                        });
+                      },
+                    ),
                   ],
                 ),
               ],
@@ -235,28 +242,31 @@ class _QuoteState extends State<Quote> {
                       height: 15,
                     ),
                     InkWell(
-                        onTap: () {
-                          /// TODO: change margin
-                        },
-                        child: SizedBox(
-                          height: 50,
-                          width: 120,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.transparent,
-                              border:
-                                  Border.all(width: 1.5, color: Colors.white30),
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            child: const Center(
-                              child: Text(
-                                '5.00%',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 18,),
+                      onTap: () {
+                        /// TODO: change margin
+                      },
+                      child: SizedBox(
+                        height: 50,
+                        width: 120,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            border:
+                                Border.all(width: 1.5, color: Colors.white30),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              '5.00%',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
                               ),
                             ),
                           ),
-                        ),),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
                 Column(
@@ -299,42 +309,51 @@ class _QuoteState extends State<Quote> {
               padding: const EdgeInsets.all(20),
               width: MediaQuery.of(context).size.width,
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(
-                    color: Colors.grey,
-                  ),),
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(
+                  color: Colors.grey,
+                ),
+              ),
               child:
                   Column(mainAxisAlignment: MainAxisAlignment.start, children: [
                 price
-                    ? const Text("25,33,118.00",
+                    ? const Text(
+                        "25,33,118.00",
                         style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 22,
-                            fontWeight: FontWeight.w600,),)
-                    : const Text("BTC 0.3211214",
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      )
+                    : const Text(
+                        "BTC 0.3211214",
                         style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 22,
-                            fontWeight: FontWeight.w600,),),
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
                 const SizedBox(
                   height: 25,
                 ),
                 SizedBox(
                   width: MediaQuery.of(context).size.width * 0.45,
                   child: ElevatedButton(
-                      onPressed: () {
-                        /// TODO: send quote
-                      },
-                      child: const Text(
-                        "Send Quote",
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
+                    onPressed: () {
+                      /// TODO: send quote
+                    },
+                    child: const Text(
+                      "Send Quote",
+                      style: TextStyle(
+                        fontSize: 18,
                       ),
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.black,
-                          elevation: 10,
-                          shape: const StadiumBorder(),),),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.black,
+                      elevation: 10,
+                      shape: const StadiumBorder(),
+                    ),
+                  ),
                 ),
                 const SizedBox(height: 10),
                 const Text(
@@ -346,20 +365,31 @@ class _QuoteState extends State<Quote> {
                 ),
                 Center(
                   child: Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          border: Border.all(color: Colors.grey, width: 1,),),
-                      child: TextFormField(
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 18),
-                        autovalidateMode: AutovalidateMode.always,
-                        decoration: const InputDecoration(
-                            hintText: 'Mobile Number',
-                            hintStyle: TextStyle(
-                                color: Colors.white54,
-                                fontSize: 18,
-                                fontWeight: FontWeight.w600,),),
-                      ),),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(5),
+                    child: TextFormField(
+                      style: const TextStyle(color: Colors.white, fontSize: 18),
+                      autovalidateMode: AutovalidateMode.always,
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.phone,
+                    inputFormatters: [],
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Mobile Number',
+                        hintStyle: TextStyle(
+                          color: Colors.white54,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
                 const SizedBox(
                   height: 20,
